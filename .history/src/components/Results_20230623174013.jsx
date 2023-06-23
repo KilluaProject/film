ce0ed1140569d164774787867ac2d8c3b7430a31
Raden Agung Prakoso Wibowo <134472737+KilluaProject@users.noreply.github.com>
2023-06-23 17:45:55 +0700

    debugging vercel-2

---
+++ .history/src/components/Results_20230623174013.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+export default function Results({results}) {
+  return (
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-col-5 max-w-6xl mx-auto px-4 py-2 space-y-2 :'>
+       {results.map((result) => (
+          <Card key={result.id} result={result}/> 
+       ))}
+    </div>
+  )
+}
